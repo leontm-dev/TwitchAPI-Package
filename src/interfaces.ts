cabdce46bb22e0720b648e37e050ea38ea422254
Leon™ <leontm.dev@gmail.com>
2023-09-26 17:03:03 +0200

    Added the getExtensionAnalyticsResponse Interface and exported it

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -7,4 +7,17 @@ interface startCommercialResponse {
     }
   ];
 }
-export { startCommercialResponse };
+interface getExtensionAnalyticsResponse {
+  data: [
+    {
+      extension_id: string;
+      URL: string;
+      type: string;
+      date_range: {
+        started_at: string;
+        ended_at: string;
+      };
+    }
+  ];
+}
+export { startCommercialResponse, getExtensionAnalyticsResponse };
